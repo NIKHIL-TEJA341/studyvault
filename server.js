6fd727772a2eaccb10a9903a6f7bb25e36706358
NIKHIL-TEJA341 <nikhilteja341@gmail.com>
2026-01-09 13:46:40 +0530

    changes made

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const Question = require("./models/Question");
 const Answer = require("./models/Answer");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const JWT_SECRET = "study_platform_secret";
+const JWT_SECRET = process.env.JWT_SECRET;
 const { verifyToken, verifyAdmin } = require("./middleware/auth");
 
 mongoose.connect(process.env.MONGO_URI)
